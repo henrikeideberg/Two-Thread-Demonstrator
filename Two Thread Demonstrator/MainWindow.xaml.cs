ab31e08feb9a63767c173db188feb74786302bb4
Henrik Eideberg <henrik@eideberg.se>
2017-12-15 00:01:54 +0100

    Create delegates for invoking gui updates in main thread. Create the threads for calculating textblock data and polygon rotation angle. Create thread abort functionality via button and MainWindow closing.

--- Two Thread Demonstrator/MainWindow.xaml.cs
+++ Two Thread Demonstrator/MainWindow.xaml.cs
@@ -22,106 +22,148 @@ namespace Two_Thread_Demonstrator
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		int angle = 0;
-		private delegate void DisplayDelegate(int x, int y, string text);
-		private DisplayDelegate delDisplay;
+		private delegate void DisplayThreadNameDelegate(int x, int y, string text); //Delegate for invoking gui updates in main thread
+		private delegate void DisplayRotatedTriangleDelegate(int angle); //Delegate for invoking gui updates in main thread
+		private DisplayThreadNameDelegate delDisplayThreadName;
+		private DisplayRotatedTriangleDelegate delDisplayRotatedTriangle;
+		private Thread displayNameThread;
+		private Thread rotateTriangle;
+		private int maxThreadIterations = 500;
 
 		public MainWindow()
 		{
 			InitializeComponent();
 		}
 
-		private void buttonStartDisplayinh_Click(object sender, RoutedEventArgs e)
+		/// <summary>
+		/// Method which run when buttonStartDisplaying is clicked. 
+		/// This method will start GetDisplayThreadNamePropertiesFromOtherThread on a thread,
+		/// (different than main thread).
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void buttonStartDisplaying_Click(object sender, RoutedEventArgs e)
 		{
-			//CreateTextBlockMainThread("ThreadName");
-
-			Thread newThread = new Thread(CreateTextBlockOtherThread);
-			newThread.SetApartmentState(ApartmentState.STA);
-			newThread.Start("Henrik");
+			displayNameThread = new Thread(GetDisplayThreadNamePropertiesFromOtherThread);
+			displayNameThread.Start("Henrik");
 		}
 
+		/// <summary>
+		/// Method which run when buttonStartRotate is clicked.
+		/// This method will start CreateRotatedTriangleOtherThread on a thread,
+		/// (different than main thread).
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void buttonStartRotate_Click(object sender, RoutedEventArgs e)
 		{
-			CreateRotatedPolygon(this.angle);
-			this.angle += 5;
+			rotateTriangle = new Thread(CreateRotatedTriangleOtherThread);
+			rotateTriangle.Start();
 		}
 
-		private void CreateTextBlockOtherThread(object name)
+		/// <summary>
+		/// Method to set the properties of a textblock.
+		/// Properties set are;
+		///  - text to disply/show
+		///  - x and y coordinates for the textblock
+		/// </summary>
+		/// <param name="name"></param>
+		private void GetDisplayThreadNamePropertiesFromOtherThread(object name)
 		{
 			Random rnd = new Random();
 			int y;
 			int x;
 			string threadName = Convert.ToString(name);
 			int i = 0;
-			while(i < 5)
+			while(i < maxThreadIterations)
 			{
 				y = rnd.Next(300);
 				x = rnd.Next(300);
 
 				/* 
-				 * Below commented out code causes exception at textBlock because
+				 * If I create textblock in thish thread, then 
+				 * below code causes exception at textBlock because
 				 * textblock is not created in main thread.
 				 */
 				//Application.Current.Dispatcher.BeginInvoke(
 				//  DispatcherPriority.Background,
 				//  new Action(() => this.canvasDisplayThreadName.Children.Add(textBlock)));
-				delDisplay = new DisplayDelegate(UpdateCanvasDisplayThreadName);
-				Dispatcher.BeginInvoke(delDisplay, new object[] { x, y, threadName });
+
+				/*
+				 * Use the display delegate to draw on the canvas from main thread.
+				 * From my understanding we cannot pass GUI elements between threads,
+				 * hence this thread creates the properties of the textblock and
+				 * pass them to the main thread (via the delegate).
+				 * The textblock is then created by the main thread 
+				 * (with the data/properties from this thread) and drawn on the canvas.
+				 */
+				delDisplayThreadName = new DisplayThreadNameDelegate(UpdateCanvasDisplayThreadName);
+				Dispatcher.BeginInvoke(delDisplayThreadName, new object[] { x, y, threadName });
+
+				//Sleep the thread for one second and then step i
 				Thread.Sleep(1000);
 				i++;
 			}
 
 		}
 
+		/// <summary>
+		/// Method to set rotation angle of triangle
+		/// </summary>
+		private void CreateRotatedTriangleOtherThread()
+		{
+			int i = 0;
+			int angle = 0;
+			while(i < maxThreadIterations)
+			{
+				//Use display delegate to draw rotated triangle on canvas from main thread
+				delDisplayRotatedTriangle = new DisplayRotatedTriangleDelegate(CreateRotatedPolygon);
+				Dispatcher.BeginInvoke(delDisplayRotatedTriangle, new object[] { angle });
+
+				//Sleep thread for one second and then step i and angle.
+				Thread.Sleep(1000);
+				i++;
+				angle = (angle + 5) % 360;
+			}
+		}
+
+		/// <summary>
+		/// Method that
+		///  - runs on main thread
+		///  - clear the canvas canvasDisplayThreadName of all children
+		///  - add a textblock to same canvas
+		/// </summary>
+		/// <param name="x"></param>
+		/// <param name="y"></param>
+		/// <param name="text"></param>
 		private void UpdateCanvasDisplayThreadName(int x, int y, string text)
 		{
 			this.canvasDisplayThreadName.Children.Clear();
-
 			TextBlock textBlock = new TextBlock();
-			textBlock.Text = text;// "Thread Fucking Name"; //Convert.ToString(name);
-
+			textBlock.Text = text;
 			textBlock.Foreground = new SolidColorBrush(Colors.Black);
-
 			Canvas.SetLeft(textBlock, x);
 			Canvas.SetTop(textBlock, y);
 			this.canvasDisplayThreadName.Children.Add(textBlock);
 		}
 
-		private void CreateTextBlockMainThread(string name)
-		{
-			canvasDisplayThreadName.Children.Clear();
-
-			Random rnd = new Random();
-			int y = rnd.Next((int)canvasDisplayThreadName.Height);
-			int x = rnd.Next((int)canvasDisplayThreadName.Width);
-
-			TextBlock textBlock = new TextBlock();
-			textBlock.Text = name;
-
-			textBlock.Foreground = new SolidColorBrush(Colors.Black);
-
-			Canvas.SetLeft(textBlock, x);
-			Canvas.SetTop(textBlock, y);
-
-			canvasDisplayThreadName.Children.Add(textBlock);
-		}
-
 		/// <summary>
-		/// //https://docs.microsoft.com/en-us/dotnet/framework/wpf/graphics-multimedia/shapes-and-basic-drawing-in-wpf-overview
+		/// Method that
+		///  - runs on main thread
+		///  - clears the canvas canvasRotateTriangle of all children
+		///  - adds a polygon (in the shape of a triangle) to same canavs 
+		/// https://docs.microsoft.com/en-us/dotnet/framework/wpf/graphics-multimedia/shapes-and-basic-drawing-in-wpf-overview
 		/// </summary>
 		/// <param name="angle"></param>
 		private void CreateRotatedPolygon(int angle)
 		{
 			canvasRotateTriangle.Children.Clear();
-
 			PointCollection myPointCollection = new PointCollection();
 			myPointCollection.Add(new Point(0, 0));
 			myPointCollection.Add(new Point(0, 1));
 			myPointCollection.Add(new Point(1, 1));
-
 			Polygon myPolygon = new Polygon();
 			myPolygon.Points = myPointCollection;
-			myPolygon.Fill = Brushes.Blue;
 			myPolygon.Width = 100;
 			myPolygon.Height = 100;
 			myPolygon.Stretch = Stretch.Fill;
@@ -131,11 +173,57 @@ namespace Two_Thread_Demonstrator
 			Canvas.SetLeft(myPolygon, left);
 			double top = (canvasRotateTriangle.ActualHeight - 100) / 2;
 			Canvas.SetTop(myPolygon, top);
-
 			RotateTransform rotateTransform1 = new RotateTransform(angle, 0, 0);
 			myPolygon.RenderTransform = rotateTransform1;
-
 			canvasRotateTriangle.Children.Add(myPolygon);
+		}
+
+		/// <summary>
+		/// Method which runs when button buttonStopDisplaying is clicked.
+		/// Will abort the thread displayNameThread if running.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void buttonStopDisplaying_Click(object sender, RoutedEventArgs e)
+		{
+			if (displayNameThread != null)
+			{
+				if (displayNameThread.IsAlive) displayNameThread.Abort();
+			}
+		}
+
+		/// <summary>
+		/// Method which runs when button buttonStopRotate is clicked.
+		/// Will abort the thread rotateTriangle if running.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void buttonStopRotate_Click(object sender, RoutedEventArgs e)
+		{
+			if(rotateTriangle != null)
+			{
+				if (rotateTriangle.IsAlive) rotateTriangle.Abort();
+			}
+		}
+
+		/// <summary>
+		/// Method which runs when application is closing.
+		/// Will abort the threads rotateTriangle and displayNameThread if running
+		/// and display a "Bye" message box.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void CheckThreads(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			if (rotateTriangle != null)
+			{
+				if (rotateTriangle.IsAlive) rotateTriangle.Abort();
+			}
+			if (displayNameThread != null)
+			{
+				if (displayNameThread.IsAlive) displayNameThread.Abort();
+			}
+			MessageBoxResult result = MessageBox.Show("Bye");
 		}
 	}
 }
